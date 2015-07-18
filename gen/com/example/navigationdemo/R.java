f710c0a9278874fe081fd4b3a60f281c8398f7ec
Allison <allison@Fuanxts-Mac-mini.local>
2015-07-18 11:55:29 +0800

    add screenshot

--- gen/com/example/navigationdemo/R.java
+++ gen/com/example/navigationdemo/R.java
@@ -1211,6 +1211,7 @@ containing a value of this type.
         public static final int content_item=0x7f03001a;
         public static final int drawer_layout=0x7f03001b;
         public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int title=0x7f03001d;
     }
     public static final class menu {
         public static final int drawer_layout_test=0x7f0d0000;
